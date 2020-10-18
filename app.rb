2178cfde7904703c0fc0bdce907d882a08f7f95a
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-18 20:52:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,9 +12,6 @@ class App < Sinatra::Base
   
   post '/puppy' do 
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
-    #@name = 
-    #@breed = 
-    #@age = 
     erb :display_puppy
   end
 
